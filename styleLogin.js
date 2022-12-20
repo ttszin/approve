088b598d370a9211da9003614ac9bf2d750ab95e
Matheus Teixeira de Souza <95655266+ttszin@users.noreply.github.com>
2022-12-20 16:02:35 -0300

    Create styleLogin.js

---
+++ styleLogin.js
@@ -0,0 +1,68 @@
+import { StyleSheet} from 'react-native';
+
+export default styles = StyleSheet.create({
+
+    fundo:{
+        flex:1,
+        backgroundColor:'#0043ad',
+        textAlign:'center',
+        borderWidth:3,
+        borderColor:'#0a0eff',
+        width:'100%',
+        flexDirection:'column',
+        
+    },
+
+    texto:{
+        color:'#ffffff',
+        fontSize:28,
+        fontFamily:'FuzzyBubbles_700Bold',
+        width:'100%',
+        textAlign:'center',
+        paddingBottom:30,
+    },
+
+    texto2:{
+        color:'#ffffff',
+        fontSize:16,
+        fontFamily:'FuzzyBubbles_400Regular',
+        textAlign:'center',
+        marginBottom:10
+    },
+
+    textoinput:{
+        width:250,
+        height:40,
+        marginRight:10,
+        borderWidth:1,
+        borderColor:'white',
+        textAlign:'center',
+        color:'white',
+    },
+
+    box: {
+        bottom: 0,
+        left: 0,
+        backgroundColor: '#0d1dd4',
+        borderColor:'#fff',
+        borderWidth:3,
+        borderRadius:10,
+    },
+    
+    boxRedes:{
+        backgroundColor:'#ffffff'
+    },
+
+    buttonLogin:{
+        borderColor:'green',
+        borderWidth:10,
+        borderRadius:10,
+        backgroundColor:'green'
+    },
+
+
+
+
+
+
+});
