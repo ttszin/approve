def95e9fe58c46c7cc21ad03504ac8d93b0620b8
Matheus Teixeira de Souza <95655266+ttszin@users.noreply.github.com>
2022-12-20 16:01:13 -0300

    Create firebase.js

---
+++ firebase.js
@@ -0,0 +1,18 @@
+import firebase from 'firebase/compat/app';
+import 'firebase/compat/auth';
+import 'firebase/compat/firestore';
+
+
+const firebaseApp = firebase.initializeApp({
+    apiKey: "AIzaSyDPdpkdoHOp98BfML2gZn-0QDUSMKIQ8Do",
+    authDomain: "approve-1435e.firebaseapp.com",
+    projectId: "approve-1435e",
+    storageBucket: "approve-1435e.appspot.com",
+    messagingSenderId: "1023652487015",
+    appId: "1:1023652487015:web:de0c7282cca2577fd899c2",
+    measurementId: "G-9924TEBQ5B"
+  });
+
+    const db = firebase.firestore();
+
+    export {db};
